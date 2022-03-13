9910bb6332c44a5d819a2097ee1c6aff596fa74f
akankshalanjewar <akankshalanjewar02@gmail.com>
2022-03-13 16:24:03 +0530

    [akanksha]added

---
+++ UC-1-StartingPosition/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace UC_1_StartingPosition
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int player = 1;
+            int StartingPosition = 0;
+            
+            Console.WriteLine("Number of Player " + player);
+            Console.WriteLine("Position at start: " + StartingPosition);
+            Console.ReadLine();
+        }
+    }
+   }
+
