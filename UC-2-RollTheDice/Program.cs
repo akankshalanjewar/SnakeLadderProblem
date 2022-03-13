d0869e9926f1dc852e3d18781c0d181a61f7872c
akankshalanjewar <akankshalanjewar02@gmail.com>
2022-03-13 16:47:41 +0530

    [Akanksha] Added file

---
+++ UC-2-RollTheDice/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace UC_2_RollTheDice
+{
+     public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int player = 1;
+            int StartingP = 0;
+
+            Random random = new Random();
+            int DiceRoll = random.Next(1, 7);
+
+            Console.WriteLine("No. of Player " + player);
+
+            Console.WriteLine("You got number :" + DiceRoll);
+            Console.ReadLine();
+        }
+    }
+    }
+
